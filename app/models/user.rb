b5719a8c095b892ebc131e8505d02c5843943aa9
thabullfrog <th3bullfrog@gmail.com>
2017-04-04 14:17:26 -0400

    add user model

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  before_save { email.downcase! }
+  validates :name, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :email, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A^(?!.*\.{2})[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\z/i
+  validates :email, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+end
