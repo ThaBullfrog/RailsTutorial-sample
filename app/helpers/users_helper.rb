279d1c07ba6808a8b0f976d0c0533691dc138279
thabullfrog <th3bullfrog@gmail.com>
2017-04-04 16:24:55 -0400

    add user profile page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://s.gravatar.com/avatar/#{gravatar_id}"
+    return image_tag(gravatar_url, alt: "#{user.name}'s Avatar", class: "gravatar")
+  end
+
 end
