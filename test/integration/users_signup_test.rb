caa8a3782fcc6731de51d8656fed37b131930f69
thabullfrog <th3bullfrog@gmail.com>
2017-04-04 21:33:39 -0400

    add tests for user signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
+
   test "users should not be able to sign up with an invalid form submission" do
     get signup_path
     assert_no_difference 'User.count' do
@@ -10,4 +11,15 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select "div#error_explanation"
     assert_select "div.field_with_errors"
   end
+
+  test "users should be able to sign up with a valid form submission" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: {user: { name: "Test User", email: "test@example.com", password: "foobar", password_confirmation: "foobar" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?, 'should show flash indicating success'
+  end
+
 end
