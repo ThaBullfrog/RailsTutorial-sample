82fb96f58ecf10c8974fc949b4de21f08cc86c3d
thabullfrog <th3bullfrog@gmail.com>
2017-06-23 14:10:35 -0400

    add image upload

--- test/integration/posts_interface_test.rb
+++ test/integration/posts_interface_test.rb
@@ -10,6 +10,7 @@ class PostsInterfaceTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
     get root_path
     assert_select 'div.pagination'
+    assert_select 'input[type=file]'
     # Invalid submission
     assert_no_difference 'Post.count' do
       post posts_path, params: { post: { content: "" } }
@@ -17,9 +18,11 @@ class PostsInterfaceTest < ActionDispatch::IntegrationTest
     assert_select 'div#error_explanation'
     # Valid submission
     content = "This post really ties the room together"
+    picture = fixture_file_upload('rails_logo.png', 'image/png')
     assert_difference 'Post.count', 1 do
-      post posts_path, params: { post: { content: content } }
+      post posts_path, params: { post: { content: content, picture: picture } }
     end
+    assert assigns(:post).picture?
     assert_redirected_to root_url
     follow_redirect!
     assert_match content, response.body
