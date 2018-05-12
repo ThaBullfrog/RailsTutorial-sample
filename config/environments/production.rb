47a1ec79ebaa0506750cf52dd83a5da1a24d3d40
ThaBullfrog <th3bullfrog@gmail.com>
2018-05-12 18:45:35 -0400

    apply small fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'bullfrogwebsites.com/sample-app'
+  host = 'bullfrogwebsites.com/sample_app'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.gmail.com',
